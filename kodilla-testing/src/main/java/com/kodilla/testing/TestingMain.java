8583b32a1c7ee1930d9f4f4afe6f8030ff26e8f9
Gregory01 <grzegorz.lach@gmail.com>
2019-03-22 19:39:44 +0100

    New commit from Gregory: Frid 22.03.2019 @19.40

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,41 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error");
+        }
+
+        //kolejny test
+        System.out.println("Test: pierwszy test jednostkowy addAtoB:");
+
+        Calculator calc1 = new Calculator();
+        int result1 = calc1.addAToB(5, 5);
+
+        if (result1 == 10) {
+            System.out.println("Test addAtoB is OK");
+        } else {
+            System.out.println("Error");
+        }
+        //kolejny test
+        System.out.println("Test: drugi test jednostkowy subtractAFromB");
+
+        Calculator calc2 = new Calculator();
+        int result2 = calc2.subtractAFromB(10, 5);
+
+        if (result2 == 5) {
+            System.out.println("Test subtractAFromB is OK");
+        } else {
+            System.out.println("Error");
+        }
     }
 }
